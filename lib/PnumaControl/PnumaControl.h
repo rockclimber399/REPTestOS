ed251bd6b11855ea36bf698cb62b53900cfcf086
Bennett <bennett.dahl@gmail.com>
2021-01-31 17:12:21 -0800

    getting there

--- lib/PnumaControl/PnumaControl.h
+++ lib/PnumaControl/PnumaControl.h
@@ -24,26 +24,28 @@ class PnumaControl
 private:
   // bool push = false; //boolean for push enable
   // bool pull = false; //boolean for pull enable
-  int pushPin; //arduino pin for push solenoid
-  int pullPin; //arduino pin for pull solenoid
+  int pushPin;          //arduino pin for push solenoid
+  int pullPin;          //arduino pin for pull solenoid
+  bool running = false; //trigger for actuation
 public:
   PnumaControl(int pushIn, int pullIn);
   ~PnumaControl();
 
-  int pushPin;                     //arduino pin for push solenoid
-  int pullPin;                     //arduino pin for pull solenoid
-  int cyclesPerSecond;             //rate of actuation
-  bool running = false;            //trigger for actuation
+  int cyclesPerSecond; //rate of actuation
+
   unsigned long baseClock;         //timer
   unsigned long lastActuation = 0; //time of last actuation
   ActuatorState status = off;      //used for tracking actuator state
   ActuatorMode mode = none;        //used for tracking actuation mode
 
-  void setup();                                   //function to set pins of object on boot.
-  void control();                                 //primary control handler, must run in loop
-  void setMode(bool pushEnable, bool pullEnable); //sets ActuatorMode based off of settings
-  void setNextCycle();                            //sets the state of the actuator's next movement based on push/pull enables and current state
-  void actuate();                                 //handles actuator state logic to fire actuator as needed
+  void setup();                                     //function to set pins of object on boot.
+  void control();                                   //primary control handler, must run in loop
+  String setMode(bool pushEnable, bool pullEnable); //sets ActuatorMode based off of settings
+  void setNextCycle();                              //sets the state of the actuator's next movement based on push/pull enables and current state
+  void actuate();                                   //handles actuator state logic to fire actuator as needed
+  bool setRunning(bool);
 };
+
+extern PnumaControl pnuma1;
 
 #endif
--- lib/REPTestOS/REPTestOS.cpp
+++ lib/REPTestOS/REPTestOS.cpp
@@ -3,7 +3,6 @@
 
 //global variable definitions
 Util util;
-PnumaControl pnuma1(9, 10);
 
 REPTestOS::REPTestOS(){};  //constructor
 REPTestOS::~REPTestOS(){}; //destructor
@@ -14,21 +13,15 @@ void REPTestOS::bootOS() //boot operating system function. To run in startup
   setupMenu();               //Initialize menu system
   Serial.begin(9600);        // initialize serial communication at 9600 bits per second:
   Serial.println("Booting"); //small serial indicator during boot phase
-
+  pnuma1.setup();            //setup function for pins for pnuma 1
   //Start remaining items
 }
 
 void REPTestOS::runOS() //constantly runs in loop
 {
   taskManager.runLoop(); //handler for menu system task management
+  pnuma1.control();
 }
-
-uint8_t taskId = taskManager.scheduleOnce(3000, doThing);
-
-void doThing()
-{
-  menuTotalCycles.setTextValue(util.returnCharPFromBool(menuTestSettingsPull.getBoolean()));
-};
 
 /*----------------------------------------------------------
 ------------------------------------------------------------
@@ -39,26 +32,31 @@ void doThing()
 void CALLBACK_FUNCTION cycleFrequency(int id)
 {
   pnuma1.cyclesPerSecond = int(menuTestSettingsCyclesPerSecond.getAsFloatingPointValue()); //update pnuma1 with value
+  Serial.println(pnuma1.cyclesPerSecond);
 }
 
 void CALLBACK_FUNCTION pullControl(int id)
 {
-  pnuma1.setMode(menuTestSettingsPush.getBoolean(), menuTestSettingsPull.getBoolean()); //update pnuma1 with mode
+  String mode = pnuma1.setMode(menuTestSettingsPush.getBoolean(), menuTestSettingsPull.getBoolean()); //update pnuma1 with mode
+  Serial.println(mode);
 }
 
 void CALLBACK_FUNCTION pushControl(int id)
 {
-  pnuma1.setMode(menuTestSettingsPush.getBoolean(), menuTestSettingsPull.getBoolean()); //update pnuma1 with mode
+  String mode = pnuma1.setMode(menuTestSettingsPush.getBoolean(), menuTestSettingsPull.getBoolean()); //update pnuma1 with mode
+  Serial.println(mode);
 }
 
 void CALLBACK_FUNCTION stopTest(int id)
 {
-  pnuma1.running = false;
+  bool running = pnuma1.setRunning(false);
+  Serial.println(running);
 }
 
 void CALLBACK_FUNCTION startTest(int id)
 {
-  pnuma1.running = true;
+  bool running = pnuma1.setRunning(true);
+  Serial.println(running);
 }
 
 void CALLBACK_FUNCTION maxRunTime(int id)
@@ -73,7 +71,8 @@ void CALLBACK_FUNCTION maxCycles(int id)
 
 void CALLBACK_FUNCTION saveTestSettings(int id)
 {
-  // TODO - your menu change code
+  pnuma1.setNextCycle();
+  pnuma1.actuate();
 }
 
 /*----------------------------------------------------------
